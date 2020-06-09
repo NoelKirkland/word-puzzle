4851fac68baa6143374a3927714e7132c4a9d96c
David Cole <davidtcole@yahoo.com>
2020-06-09 14:17:41 -0700

    added a function sansVowels which takes a string as an argument. Within the function we created a vowels variable and for loop which contains an if statement. For the comparison, admittedly we looked everywhere for solutions and found one that sort of fit our needs and re-factored it. The indexOf() method returns the index position where a match is found. If no match is found, it returns -1. If our condition returns anything besides -1, we are replacing the value at that index with a -
    Co-authored-by: Noel Kirkland <noelkirkland@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -3,18 +3,22 @@ $(document).ready(function() {
     event.preventDefault();
 
     let inputSentence = $("input#sentence").val();
-    const inputWords = inputSentence.split(" ");
+    let result = sansVowels(inputSentence);
+    alert(result);
 
-    function sansVowels(inputWords) {
-      let sentenceWithoutVowels = []
-      const vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
-      for(index = 0; index < inputWords.length; index += 1) {
-        if (vowels.index(string[index])<0){
-          sentenceWithoutVowels.push(string[index])
-        }
-      }
-    }
-console.log();
-    
   });
 });
+
+function sansVowels(inputSentence) {
+  let sentenceWithoutVowels = [];
+  const vowels = "aeiou";
+
+  for (var i = 0; i < inputSentence.length; i += 1) {
+    if (vowels.indexOf(inputSentence[i]) > -1) {
+      inputSentence = inputSentence.replace(inputSentence[i], "-");
+    }
+  }
+  console.log(inputSentence);
+  sentenceWithoutVowels = inputSentence.split(" ");
+  return sentenceWithoutVowels;
+};
